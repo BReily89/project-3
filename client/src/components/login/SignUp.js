3fc0f528fb59a3ab1a6b1f6b4f2b6616f675d10c
BReily89 <BrandonReily.br@gmail.com>
2017-10-15 14:26:58 -0400

    building sign up page

--- client/src/components/login/SignUp.js
+++ client/src/components/login/SignUp.js
@@ -14,12 +14,31 @@ class SignUp extends Component {
             const attribute = event.target.userName
             const updateUser =  {...this.state.newUser}
 
+            updateUser[attribute] = event.target.value
+            this.setState({newUser: updateUser})
+        }
+        handleSubmit = async (event) => {
+            event.preventDefault()
+
+            const res = await axios.post('/api/users', {
+                'user': this.state.newUser
+            })
+            this.setState({redirectToExerciseLibrary: true, newUserId: res.data._id})
         }
     render() {
+
+        if (this.state.redirectToExerciseLibrary) {
+            return <redirect to={'/exercises/${this.state.newUSerId'} />
+        }
         return (
             <div>
-                
+                <h1>Sign Up</h1>
+                <form onSubmit={this.handleSubmit}/>
             </div>
+            <label htmlFor="userName">User Name</label>
+            <input
+            onChange={this.handleChange} name="userName"
+            type="text" value={this.state.newUser.userName}
         );
     }
 }
