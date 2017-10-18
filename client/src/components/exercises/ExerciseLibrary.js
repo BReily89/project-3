04d1815cbe3dcf150028e3adaba724b282d1e081
BReily89 <BrandonReily.br@gmail.com>
2017-10-17 21:38:15 -0400

    wokring on exercise route/seeds

--- client/src/components/exercises/ExerciseLibrary.js
+++ client/src/components/exercises/ExerciseLibrary.js
@@ -63,8 +63,8 @@ class ExerciseLibrary extends Component {
      return (
          <div> 
              <h1>{this.state.name}'s Exercise Library</h1>
-             {this.state.exercise.map(exercise => {
-             return (<Link key={exercise._id} to={`/exercies/${exercise._id}/exercises`}>{exercise.name}</Link>)}) 
+            {/*} {this.state.exercise.map(exercise => { */}
+            {/*} return (<Link key={exercise._id} to={`/exercies/${exercise._id}/exercises`}>{exercise.name}</Link>)}) */}
              })}
              {this.state.exercises[0]}
 
